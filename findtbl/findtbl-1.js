cf715c1ce9e5931a043b5ce7de9a9f70a64a221a
slowbyte <slowbyte1@gmail.com>
2021-01-21 09:53:18 -0500

    added clk event to all flexboxes in grid col 1... and when they r clked bkgnd color goes to WHITE

--- findtbl/findtbl-1.js
+++ findtbl/findtbl-1.js
@@ -4,17 +4,34 @@ function FTpg1Run()
     //alert("FTpg1Run()");
     currentFindTblpage = 1;
     objNAVfindtblpg1.style.display = "flex";
-    clkClassA();
+    
+     clkClassA();
+
 }
+
+
+
 
 function clkClassA()
 {
+    //var z = document.getElementById("a1");
+    /*var z = document.getElementById("a1");
+    alert(z);
+    z.addEventListener("click" , flexboxClked);
+*/
    var elemsA =   document.getElementsByClassName("a");
    for (var x of elemsA)
     {
         console.log(x.id);
-        alert(x.className);
-
+        //alert(x.className);
+        //x.style.backgroundColor = "blue"; //WORKS OK...
+        x.addEventListener("click", flexboxClked);
   }
-  
+}
+
+function flexboxClked()
+{
+    console.log("clicked");
+   // alert("flexbox clicked" + "/ " + this.id);
+    this.style.backgroundColor = "white";
 }
